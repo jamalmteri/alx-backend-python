89dadb5239b699f9fd31770655bbdf3c573b6870
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2023-07-11 16:22:55 +0300

    async_generator

---
+++ 0x01-python_async_function/0-async_generator.py
@@ -0,0 +1,15 @@
+import asyncio
+import random
+
+async def async_generator():
+    for _ in range(10):
+        await asyncio.sleep(1)  # Asynchronously wait for 1 second
+        yield random.randint(0, 10)  # Generate and yield a random number between 0 and 10
+
+# Example usage
+async def main():
+    async for number in async_generator():
+        print(number)
+
+asyncio.run(main())
+
