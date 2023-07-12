710a8a5569d9bdd0f8658bebf76a2cee39dc5874
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2023-07-12 16:28:17 +0300

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-
+'''Task 0's module.
+'''
 import asyncio
 import random
+from typing import Generator
 
-async def async_generator():
+
+async def async_generator() -> Generator[float, None, None]:
+    '''Generates a sequence of 10 numbers.
+    '''
     for _ in range(10):
-        await asyncio.sleep(1)  # Asynchronously wait for 1 second
-        yield random.randint(0, 10)  # Generate and yield a random number between 0 and 10
-
-# Example usage
-async def main():
-    async for number in async_generator():
-        print(number)
-
-asyncio.run(main())
-
+        await asyncio.sleep(1)
+        yield random.random() * 10
