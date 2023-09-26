d94c6bd015c57160e989003b6701b4b22346ff04
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2023-09-26 09:56:27 +0000

    compehesion

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
-'''Task 0's module.
-'''
+"""
+This module provides an asynchronous generator function that yields a
+random float between 0 and 10 after a one second delay for a total of
+10 iterations.
+"""
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    '''Generates a sequence of 10 numbers.
-    '''
+    """
+    Asynchronous generator function that yields a random float between 0 and 10
+    after a one second delay for a total of 10 iterations.
+
+    Returns:
+        Generator: Asynchronous generator object that can be used in an
+        awaitable context.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
+        
